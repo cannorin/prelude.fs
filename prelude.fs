451ac56d8bfece213ed67a9533942007c377ab68
cannorin <cannorin@users.noreply.github.com>
2019-01-18 09:43:32 +0900

    Add implicit operator

--- prelude.fs
+++ prelude.fs
@@ -53,6 +53,8 @@ module ToplevelOperators =
   let inline dispose (disp: #System.IDisposable) =
     match disp with null -> () | x -> x.Dispose()
 
+  let inline implicit (x: ^a) : ^b = ((^a or ^b) : (static member op_Implicit: ^a -> ^b) x)
+
   let inline succ (n: ^number) =
     n + LanguagePrimitives.GenericOne< ^number >
 
